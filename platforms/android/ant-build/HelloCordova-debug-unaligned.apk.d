88dee74546f08439ea6bfce27ffa3687235339b7
tfgphonegap <pol_8_v@hotmail.com>
2014-04-16 18:35:58 +0200

    Readme modified

--- platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
+++ platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
@@ -2,3 +2,4 @@
 /home/calldidoctor/AngularJS/ionic/proves/platforms/android/ant-build/HelloCordova.ap_ \
 /home/calldidoctor/AngularJS/ionic/proves/platforms/android/ant-build/classes.dex \
 /home/calldidoctor/AngularJS/ionic/proves/platforms/android/CordovaLib/ant-build/classes.jar \
+/home/calldidoctor/AngularJS/ionic/proves/platforms/android/libs/com.google.zxing.client.android.captureactivity.jar \
